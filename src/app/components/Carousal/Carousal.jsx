d1f1933ef1ceaa2b62edb943487762fe99f6a524
Plena-Prince <prince@plena.finance>
2024-02-20 16:15:29 +0530

    img change

--- src/app/components/Carousal/Carousal.jsx
+++ src/app/components/Carousal/Carousal.jsx
@@ -46,15 +46,15 @@ export default function MyCarousel() {
     <div className="relative overflow-hidden w-full min-h-[400px] bg-white rounded-lg">
     <div className="carousel-body absolute top-0 bottom-0 start-0 flex flex-row transition-transform duration-700 opacity-100" ref={carouselRef}>
       <div className="carousel-slide w-full h-full object-scale-down flex-none active relative">
-        <img className="w-full h-full object-cover" src="https://blogs-images.forbes.com/erikkain/files/2017/11/justice-league.jpg" alt="First slide" />
+        <img className="w-full h-full object-cover object-top" src="https://blogs-images.forbes.com/erikkain/files/2017/11/justice-league.jpg" alt="First slide" />
         <div className="absolute inset-0 bg-black opacity-40"></div>
       </div>
       <div className="carousel-slide w-full h-full object-scale-down flex-none relative">
-        <img className="w-full h-full object-cover" src="https://cdn.marvel.com/content/1x/avengersendgame_lob_mas_mob_01.jpg" alt="Second slide" />
+        <img className="w-full h-full object-cover object-top" src="https://m.media-amazon.com/images/S/pv-target-images/189420c3dbf235953dbb719a513f845fe8422135b1008eadc67b8356ee4d2751.jpg" alt="Second slide" />
         <div className="absolute inset-0 bg-black opacity-40"></div>
       </div>
       <div className="carousel-slide w-full h-full object-scale-down flex-none relative">
-        <img className="w-full h-full object-cover" src="https://www.hindustantimes.com/ht-img/img/2023/03/28/1600x900/One_Piece_Wano_1679966485338_1679966497954_1679966497954.webp" alt="Third slide" />
+        <img className="w-full h-full object-cover object-top" src="https://static.tnn.in/thumb/msid-104879191,thumbsize-74204,width-1280,height-720,resizemode-75/104879191.jpg" alt="Third slide" />
         <div className="absolute inset-0 bg-black opacity-40"></div>
       </div>
     </div>
