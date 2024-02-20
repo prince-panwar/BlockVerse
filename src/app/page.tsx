fc37042b58adac167f60e93a746b31785220645f
Arpit098 <arpitagrawal3048@outlook.com>
2024-02-20 13:24:54 +0530

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,12 @@ import React from 'react'
 import ThemeSwitcher from './components/ThemeSwitcherButton'
 import {Card, CardHeader, CardBody, Image} from "@nextui-org/react";
 import Nav from './components/Navbar/Navbar'
+import Carousel from './components/Carousal/Carousal'
 export default function Home() {
   return (
     <>
     <Nav/>
-  
+    <Carousel/>
     </>
   )
 }
