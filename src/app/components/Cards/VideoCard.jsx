85a8a92d7159279e7613278a493e9ad8a5e3dea0
Arpit098 <arpitagrawal3048@outlook.com>
2024-03-20 16:47:55 +0530

    "Backend Completed"

--- src/app/components/Cards/VideoCard.jsx
+++ src/app/components/Cards/VideoCard.jsx
@@ -11,20 +11,19 @@ export default function VideoCard({id ,title,price,des,imageUrl}) {
   return (
     <div>
       <Card isPressable="true" onPress={() => handlePress()} className="py-4 h-80 w-90 mr-10 mb-10 font-sans">
-        <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-          <p className="text-xs uppercase  font-bold">{title}</p>
-          <small className="text-default-500">{price}</small>
-         
-        </CardHeader>
-        <CardBody className="overflow-visible py-2">
-          <Image
+    <CardHeader className="pb-0 pt-2 px-4 flex-col items-start w-64">
+        <p className="text-xs uppercase truncate max-w-[280px] font-bold">{title}</p>
+    </CardHeader>
+    <CardBody className="overflow-visible py-2">
+        <Image
             alt="Card background"
             className='object-cover rounded-xl h-80 '
             src={imageUrl}
             width={280}
-          />
-        </CardBody>
-      </Card>
+        />
+    </CardBody>
+</Card>
+
     </div>
   )
 }
